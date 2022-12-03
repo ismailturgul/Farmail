3aabcd1f6dfda60d1edb00240e8af9422197adaa
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-12-03 14:20:36 +0100

    Added Movement to Player & Add Player

---
+++ Islonja/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    float current_Position;
+        public float speed;
+
+private void Update()
+    { }
+
+}
+
