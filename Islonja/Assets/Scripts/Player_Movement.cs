a3bbe012a2aac07267050b819f7508780bd5f7f5
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-12-04 13:52:17 +0100

    Added Collision to the Water Tilemap & Added Apple
    need a script to handle collision

--- Islonja/Assets/Scripts/Player_Movement.cs
+++ Islonja/Assets/Scripts/Player_Movement.cs
@@ -6,18 +6,24 @@ public class Player_Movement : MonoBehaviour
 {
     public float speed;
     public Animator animator;
+    Vector3 direction;
 
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, vertical);
+        direction = new Vector3(horizontal, vertical);
 
         AnimateMovement(direction);
-        transform.position += direction * speed * Time.deltaTime;
     }
 
+
+    void FixedUpdate()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+
+    }
     void AnimateMovement(Vector3 direction)
     {
         if(animator != null)
