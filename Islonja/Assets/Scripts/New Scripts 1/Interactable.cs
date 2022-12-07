b6b401b19343102e0a39f52077cfb6cb15688c58
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-12-07 01:30:24 +0100

    Added Loot-Container for Interactables

--- Islonja/Assets/Scripts/New Scripts 1/Interactable.cs
+++ Islonja/Assets/Scripts/New Scripts 1/Interactable.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public virtual void Interact(Character character)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Islonja/Assets/Scripts/New Scripts 1/Loot_Container_Interact.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot_Container_Interact : Interactable
+{
+    [SerializeField] GameObject closed_Chest;
+    [SerializeField] GameObject opened_Chest;
+    [SerializeField] bool opened;
+
+    public override void Interact(Character charachter)
+    {
+        if (opened == false)
+        {
+            opened = true;
+            closed_Chest.SetActive(false);
+            opened_Chest.SetActive(true);
+        }
+    }
+}
