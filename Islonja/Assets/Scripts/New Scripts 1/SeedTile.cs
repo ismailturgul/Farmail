77bd77c1d88e9e3df40274693fcc574c0e48c37f
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2023-01-17 16:49:56 +0100

    Seeds needed for Seeding Implementet
    just 1 Seed yet

--- Islonja/Assets/Scripts/New Scripts 1/SeedTile.cs
+++ Islonja/Assets/Scripts/New Scripts 1/SeedTile.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Data/ToolAction/Seed Tile")]
 public class SeedTile : ToolAction
 {
     public override bool OnApplyToTileMap(Vector3Int gridposition, TileMapReadController tileMapReadController )
