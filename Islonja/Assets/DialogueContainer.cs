e1659e4eb7312dbec8f0b8fcbe666c9a90041af2
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2023-01-18 04:16:48 +0100

    Added Dialogue script to the game
    And a UI for the Dialogue Panel

---
+++ Islonja/Assets/DialogueContainer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Data/Dialogue/Dialogue")]
+public class DialogueContainer : MonoBehaviour
+{
+    public List<string> line;
+    public Actor actor;
+}
