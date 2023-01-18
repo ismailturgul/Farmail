3f7c0519c9180b8c5c3c5b00a1f479c834cd18f7
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2023-01-18 04:51:44 +0100

    Dialogue letter by letter added

--- Islonja/Assets/DialogueSystem.cs
+++ Islonja/Assets/DialogueSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,12 +14,29 @@ public class DialogueSystem : MonoBehaviour
 
     DialogueContainer currentDialogue;
     int currentTextLine;
+    [Range(0f,1f)]
+    [SerializeField] float visibleTextPercent;
+    [SerializeField] float timePerLetter = 0.0f;
+    float totalTimeToType, currentTime;
+    string lineToShow;
+
     private void Update()
     {
         if  (Input.GetMouseButtonDown(0))
         {
             PushText();
         }
+        TypeOutText();
+    }
+
+    private void TypeOutText()
+    {
+        if(currentTime >= totalTimeToType) { return; }
+        currentTime += Time.deltaTime;
+        float progress = currentTime / totalTimeToType;
+        progress = Mathf.Clamp(progress, 0, 1f);
+        int letterCount = (int)(lineToShow.Length * progress);
+        targetText.text = lineToShow.Substring(0, letterCount);
     }
 
     private void PushText()
@@ -29,19 +47,28 @@ public class DialogueSystem : MonoBehaviour
             ConClude();
         }else
         {
-            targetText.text = currentDialogue.line[currentTextLine];
+            lineToShow= currentDialogue.line[currentTextLine];
+            totalTimeToType = lineToShow.Length * timePerLetter;
+            currentTime = 0f;
+            targetText.text = "";
         }
     }
 
     public void Initialize(DialogueContainer dialogueContainer)
-    { 
+    {
+        Show(true);
         currentDialogue = dialogueContainer;
         currentTextLine = 0;
         targetText.text = currentDialogue.line[currentTextLine];
     }
 
+    private void Show(bool v)
+    {
+       gameObject.SetActive(v);
+    }
+
     private void ConClude()
     {
-        //
+        Show(false);
     }
 }
