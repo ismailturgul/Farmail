182b0d1bd85c48a7a67dabb8827bd0975194cb69
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2023-01-19 18:05:16 +0100

    Added new map

---
+++ Islonja/Assets/Transition.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Apple;
+using UnityEngine.SceneManagement;
+
+public enum TransitionType
+{
+    Warp,
+    Scene
+}
+public class Transition : MonoBehaviour
+{
+    [SerializeField] TransitionType transitionType;
+    [SerializeField] string sceneNameToTransition;
+
+    Transform destination;
+    void Start()
+    {
+        destination = transform.GetChild(1);
+    }
+
+    internal void InitiateTransition(Transform toTransition)
+    {
+        switch(transitionType)
+        {
+        case TransitionType.Warp:
+            toTransition.position = new Vector3(
+                destination.position.x,
+                destination.position.y,
+                toTransition.position.z
+                );
+                break;
+            case TransitionType.Scene:
+                SceneManager.LoadScene(sceneNameToTransition);
+                break;
+
+        }
+    }
+
+}
